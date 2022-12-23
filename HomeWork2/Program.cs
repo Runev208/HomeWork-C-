d79ffbe15d3fb30ac04d130fbef8b1deccd9574c
Smile701 <Slash208@mail.ru>
2022-12-23 22:28:57 +0300

    Добавили решение второй задачи

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Домашняя работа по Cеминару 2
 
-// Задача 1 
+// Задача 1.
 // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int cutnumber(int number) 
 {
  int ten = number / 10;
@@ -26,3 +26,39 @@ else
 {
     Console.WriteLine("Please input correct number");
 }
+*/
+
+// Задача 2.
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int cutnumber(int number)
+{
+    while (number > 999 || number < -999)
+    {
+    number = number / 10;
+    }
+    number = number % 10;
+    return number;  
+}
+int plus(int number)
+{
+  while(number < 0)
+  {
+    number = -1 * number;
+  }
+  return number;
+}
+Console.WriteLine("Please input your number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 || number <= -100 && number != 0)
+{ 
+    number = plus(number);
+    Console.WriteLine($"Third number is {cutnumber(number)}");
+}
+else
+{
+    Console.WriteLine("Please input correct number");
+}
+*/
+
+// Задача 3.
