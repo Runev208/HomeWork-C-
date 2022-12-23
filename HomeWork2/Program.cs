06fcfaa04bdc84d5bbb8e1538a31ca29306410ae
Smile701 <Slash208@mail.ru>
2022-12-23 22:23:42 +0300

    Добавили решение первой задачи

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Домашняя работа по Cеминару 2
+
+// Задача 1 
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int cutnumber(int number) 
+{
+ int ten = number / 10;
+ int remainder = ten % 10;
+ return remainder;
+}
+Console.WriteLine("Please input your number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if((-999 <= number && number <= -100) || (number <= 999 && number >= 100 && number != 0))
+{
+    if (1 <= number && number <= 999)
+    {
+    Console.WriteLine($"Second number is " + (cutnumber(number)));
+    }
+    else
+    {
+    Console.WriteLine($"Second number is " + (-1 *(cutnumber(number))));
+    }
+}
+else
+{
+    Console.WriteLine("Please input correct number");
+}
