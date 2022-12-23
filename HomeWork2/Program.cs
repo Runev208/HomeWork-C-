a829d208218964202fe4d4098949ec81b6c663e6
Smile701 <Slash208@mail.ru>
2022-12-23 22:30:41 +0300

    Добавили решение третьей задачи

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -62,3 +62,24 @@ else
 */
 
 // Задача 3.
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+bool hoilday(int numberofaday)
+{
+if (numberofaday > 7 || numberofaday <= 5 || numberofaday < 1) return false;
+return true;
+}
+
+Console.Write("Please input a number of a day (1-7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Please input correct number");
+}
+else
+{
+    bool weekend = hoilday(number);
+    if (weekend){Console.WriteLine("This is holiday!" );}
+    else{Console.WriteLine("This is not holiday!");}
+}
