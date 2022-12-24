2d1924e374360c9e0268969640d0b8643e26dd71
RudnevEV <Slash2190@gmail.com>
2022-12-24 06:04:26 +0300

    добавили решение первой задачи

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -5,7 +5,56 @@
 // является ли оно палиндромом.
 
 /*
+int getDigitAtPosition(int number, int position)
+{
+    int index = 5 - position;
 
+    if (index < 0 || index > 4)
+    {
+        return 0;
+    }
+    if (index == 0)
+    {
+        return number % 10;
+    }
+    int modulo = Convert.ToInt32(Math.Pow(10, index + 1));
+    int divider = Convert.ToInt32(Math.Pow(10, index));
+
+    return (number % modulo) / divider;
+}
+
+bool isPalindrome(int num)
+{
+    for (int position = 1; position <= 2; position++)
+    {
+        int digit = getDigitAtPosition(num, position);
+        int mirroredDigit = getDigitAtPosition(num, 6 - position);
+        if (digit != mirroredDigit)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine("Введите пятизначное число");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Недопустимое значение");
+}
+else
+{
+    if (isPalindrome(num))
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
 
 */
 
