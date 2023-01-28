398b13e0c21cd38049e984874e02e75c11fa37b5
RudnevEV <Slash2190@gmail.com>
2023-01-28 12:04:19 +0300

    Добавили решение задачи 41

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -4,3 +4,20 @@
 // 1, -7, 567, 89, 223 -> 3
 
 /*
+int GetAmountPositiveNumber(int howManyNumbers)
+{
+    int result = 0;
+    for(int count = 0; count < howManyNumbers; count++)
+    {
+        Console.Write("Пожалуйста, введите число: ");
+        if(Convert.ToInt32(Console.ReadLine()) > 0) result++;
+    }
+    return result;
+}
+Console.WriteLine("Данная программа подсчитывает количество положительных чисел.");
+Console.Write("Пожалуйста, укажите сколько чисел вы собираетесь ввести: ");
+int howManyNumbers = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество положительных чисел: {GetAmountPositiveNumber(howManyNumbers)}.");
+
+*/
+
