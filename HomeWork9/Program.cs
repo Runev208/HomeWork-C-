0e723635a103a2f2e12fd312f616381473f5d616
RudnevEV <Slash2190@gmail.com>
2023-02-04 14:45:09 +0300

    Добавили решение задачи 66

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 // N = 5. -> "5, 4, 3, 2, 1"
 // N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
- 
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
@@ -18,5 +18,40 @@ void NaturalToLow(int n, int count)
     {
         NaturalToLow(n, count + 1);
         Console.Write(count + " ");
+    }
+}
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+ 
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
     }
 }
